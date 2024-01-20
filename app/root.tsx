972bc4e4b3d66910fc195d9478b5d2ae7d72f164
rphlmr <raphael@uaerom.com>
2024-01-20 16:47:25 +0100

    tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -6,17 +6,22 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+// https://remix.run/docs/en/main/future/vite#fix-up-css-imports
+import "~/tailwind.css";
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="en" className="h-dvh bg-zinc-900 w-screen text-zinc-50">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1.0,maximum-scale=1.0,viewport-fit=cover"
+        />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="size-full">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
