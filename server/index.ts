9c2d39fdeae5a87be4549eca64d6c083700af46d
rphlmr <raphael@uaerom.com>
2024-03-15 07:30:50 +0100

    👌 IMPROVE:update deps + fight the machine

--- server/index.ts
+++ server/index.ts
@@ -79,7 +79,9 @@ app.use(
  */
 app.use(async (c, next) => {
   const build = (isProductionMode
-    ? // eslint-disable-next-line import/no-unresolved -- this expected until you build the app
+    ? // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      // eslint-disable-next-line import/no-unresolved -- this expected until you build the app
       await import("../build/server/remix.js")
     : await importDevBuild()) as unknown as ServerBuild;
 
